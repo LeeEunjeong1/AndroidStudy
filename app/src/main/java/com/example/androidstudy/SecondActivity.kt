e8df7962b121e4ba55a7e38224e85ea609e2fc87
Eunjeong <tbig1019@gmail.com>
2022-04-28 13:40:12 +0900

    머터리얼 구성요소 - TopAppBar actions

--- app/src/main/java/com/example/androidstudy/SecondActivity.kt
+++ app/src/main/java/com/example/androidstudy/SecondActivity.kt
@@ -5,10 +5,9 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,6 +42,12 @@ fun AndroidStudy() {
             TopAppBar(
                 title = {
                     Text(text = "AndroidStudyWithEJ")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Favorite,contentDescription = null)
+
+                    }
                 }
             )
         }
