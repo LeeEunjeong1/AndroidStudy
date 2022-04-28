90e5537c8780112185b48321c2a1d8272df91951
Eunjeong <tbig1019@gmail.com>
2022-04-28 15:37:06 +0900

    ImageList

--- app/src/main/java/com/example/androidstudy/SecondActivity.kt
+++ app/src/main/java/com/example/androidstudy/SecondActivity.kt
@@ -3,16 +3,21 @@ package com.example.androidstudy
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.androidstudy.ui.theme.AndroidStudyTheme
 
 class SecondActivity : AppCompatActivity() {
@@ -56,7 +61,9 @@ fun AndroidStudy() {
         }
     ) {
        // BodyContent(Modifier.padding(innerPadding))
-        SimpleList()
+       // SimpleList()
+        ImageList()
+
     }
 }
 
@@ -83,5 +90,31 @@ fun SimpleList(){
         repeat(100){
             Text("Item #$it")
         }
+    }
+}
+
+@Composable
+fun ImageList(){
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(state = scrollState){
+        items(100){
+            ImageListItem(it)
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int){
+    Row(verticalAlignment = Alignment.CenterVertically){
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Iten #$index",style = MaterialTheme.typography.subtitle1)
     }
 }
