0b4571380a7250105572dd1f0e7e9f90040f46f8
Eunjeong <tbig1019@gmail.com>
2022-04-29 14:00:40 +0900

    Recomposition을 통해 redraw를 해야 Text에서 data를 받을 수 있다. - mutablestateOf + remember

--- app/src/main/java/com/example/composestudy/TestActivity.kt
+++ app/src/main/java/com/example/composestudy/TestActivity.kt
@@ -1,7 +1,6 @@
 package com.example.composestudy
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,10 +8,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -66,16 +64,20 @@ fun DefaultPreview(){
 @Preview
 @Composable
 fun CreateCircle(){
+    var cnt by remember {
+        mutableStateOf(0)
+    }
     Card(modifier = Modifier
         .padding(3.dp)
         .size(50.dp)
         .clickable {
+            cnt += 1
             Log.d("Tap", "CreateCircle : Tap")
         },
     shape = CircleShape,
     elevation = 4.dp) {
         Box(contentAlignment = Alignment.Center){
-            Text(text = "Tap",modifier = Modifier)
+            Text(text = "Tap $cnt",modifier = Modifier)
         }
 
     }
